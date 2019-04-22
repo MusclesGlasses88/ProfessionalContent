6f7b63c0731345337c6dd8a4b953aa1e5e1819b8
MusclesGlasses88 <46330293+MusclesGlasses88@users.noreply.github.com>
2019-04-21 23:56:24 -0500

    Add files via upload

---
+++ ProfessionalWebsite/WalterJBResume.js
@@ -0,0 +1,12 @@
+
+
+const proceed = prompt('Would you like to see what I\'ve been up to? Y or N?');
+console.log('is it on?');
+if(proceed === 'Y' || proceed === 'y'){
+
+    alert('Thanks for checking me out!')}
+
+    else{
+
+        alert('Well Take A Look Anyway Since You\'re Here! Thanks!')
+    }
